d1c1b38e52f4bd7cb7f557acb18de09530021471
danicolms <danicolms@icloud.com>
2023-07-31 17:20:49 +0200

    :sparkles: add category to listitem

--- src/components/List/ListItem.js
+++ src/components/List/ListItem.js
@@ -4,14 +4,22 @@ import PropTypes from "prop-types";
 import "./styles.css";
 
 ListItem.propTypes = {
-  item: PropTypes.object.isRequired,
+  item: PropTypes.shape({
+    url: PropTypes.string.isRequired,
+    icon: PropTypes.element.isRequired,
+    category: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 function ListItem({ item }) {
   return (
     <a className="list__item" target="_blank" href={item.url}>
       <i className="list__icon">{item.icon}</i>
-      <span className="list__value"> {item.value}</span>
+      <div className="list__text-container">
+        <p className="text-container__caption"> {item.category} </p>
+        <span className="text-container__value"> {item.value}</span>
+      </div>
     </a>
   );
 }
